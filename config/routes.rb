9c388072e55b032290ab237b3fa701dbaafd171c
dylan wang <zywang_dylan@126.com>
2018-10-10 19:58:18 -0400

    new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/projects'
+  get 'pages/about'
+  get 'pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
